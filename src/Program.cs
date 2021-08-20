bfc0f1c3a29d2f210cdca8117287e8d7d3606154
Dio Mercury <dio@live.ru>
2021-08-20 14:40:34 +0300

    Add source code

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+	.AddJsonFile("appsettings.json")
+	.AddUserSecrets("930d7287-a0ec-4b4f-a966-8a70c29e4a05")
+	.AddEnvironmentVariables();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.MapGet("", async context =>
+{
+	string url = app.Configuration[$"Redirects:{context.Request.Host.Host}"];
+	if (url != null)
+	{
+		context.Response.Redirect(url);
+	}
+	else
+	{
+		await context.Response.WriteAsync("Hello world!");
+	}
+});
+
+app.Run();
